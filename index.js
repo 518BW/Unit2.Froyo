52ffac218ce96ad641532b0d4d152ba27abeeb7b
Your Name <portia894@gmail.com>
2023-11-06 11:40:50 -0500

    Outputs total entries into table

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const userInputString = prompt(
+    "Please enter some froyo flavors separated by commas.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+
+  const stringArray = userInputString.split(",");
+
+
+//Calls forEach on stringArray with a callback that puts the array item x as a property of count.
+//Updates counts[x] as the itme x is being found from iteration. counts[x] set at 0
+let counts = [];
+
+stringArray.forEach(x=>counts[x]=(counts[x] || 0)+1 );
+console.table(counts)
+
 
